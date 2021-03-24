316487823afda20a7e02eb9a215ced1ed335bdf8
Wikukarnoprasetyagama <prasetyagama2@gmail.com>
2021-03-24 11:44:32 +0700

    tambah fitur daftar

---
+++ resources/views/daftar.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <link rel="shorcut icon" href="{{ url('frontend/images/logo/uir.png')}}">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+    <title>Pendaftaran</title>
+  </head>
+  <body>
+
+    <section class="section-maintenence" style="margin-top: 150px">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-12 text-center">
+                    <figure class="figure text-center">
+                        <img src="{{ url('frontend/images/maintenence/maintenence.png') }}" class="figure-img img-fluid rounded" alt="...">
+                        <h3 class="mt-4" style="color: #004420; font-family: PlayfairDisplay; font-weight: bold; font-size: 32px">Mohon Maaf...!</h3>
+                        <p class="text-muted" style="font-size: 16px; font-family: Poppins; ">Halaman Yang Anda Akses Sedang Dalam Pembaruan</p>
+                        <a href="{{ ('/') }}" class="btn mt-3" style="font-family: Poppins; background-color: #004420; color: white;">Kembali</a>
+                      </figure>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+  </body>
+</html>
